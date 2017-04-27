bd7df9361780157910dd09d8ff9bfa97bda69e79
Anton Legoo <azeven@gmail.com>
2017-04-27 15:40:50 -0700

    basically working.

---
+++ src/app/components/main-footer.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="footer pad--v-2 col-12">
+		<p>
+			Footer
+		</p>
+	</div>
+</template>
+
+###########################################################
+
+<script>
+
+export default
+{
+	name: "main-footer",
+	data ()
+	{
+		return {}
+	},
+	// mounted: function(){}
+}
+</script>
+
+###########################################################
+
+<style lang="scss">
+
+@import "~styles/_vars.scss";
+
+//
+//  # Vars
+// 
+
+
+//
+//  # Block
+// 
+
+.footer
+{}
+
+
+//
+//  # Element
+// 
+
+
+
+</style>
