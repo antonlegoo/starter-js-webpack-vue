09bed4204e24ba4d9634c325ed9572c4d0cc4221
Anton Legoo <azeven@gmail.com>
2017-04-27 15:40:50 -0700

    basically working.

---
+++ src/app/app.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <main-header></main-header>
+        <main-content></main-content>
+        <main-footer></main-footer>
+    </div>
+</template>
+
+###########################################################
+
+<script>
+
+import header from "./components/main-header.vue";
+import content from "./components/main-content.vue";
+import footer from "./components/main-footer.vue";
+
+export default
+{
+	data ()
+	{
+		return {}
+	},
+    components: 
+    {
+        "main-header"   : header,
+        "main-content"  : content,
+        "main-footer"   : footer,
+    }
+}
+</script>
+
+###########################################################
+
+<style lang="scss">
+
+@import "~styles/_vars.scss";
+
+//
+//  # Vars
+// 
+
+
+//
+//  # Block
+// 
+
+
+//
+//  # Element
+// 
+
+</style>
---
+++ src/app/components/main-header.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="header">
+		<h1 class="header__title h-text-left col-12 pad--v-2">
+			Header
+		</h1>
+	</div>
+</template>
+
+###########################################################
+
+<script>
+
+export default
+{
+	name: "main-header",
+	data ()
+	{
+		return {}
+	},
+	// mounted: function(){}
+}
+</script>
+
+###########################################################
+
+<style lang="scss">
+
+@import "~styles/_vars.scss";
+
+//
+//  # Vars
+// 
+
+
+//
+//  # Block
+// 
+
+.header
+{}
+
+
+//
+//  # Element
+// 
+
+.header__title
+{
+	@include breakpoint( $bp-desktop )
+	{
+		color: purple;
+	}
+
+	@include breakpoint( $bp-tablet )
+	{
+		color: red;
+	}
+
+	@include breakpoint( $bp-mobile )
+	{
+		color: orange;
+	}
+}
+
+</style>
