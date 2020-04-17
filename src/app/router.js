520f82d2782fac67c422565e1f5656cdc0fbcc1b
Anton Legoo <azeven@gmail.com>
2020-04-17 10:30:49 -0700

    Added disable 'history' mode to router

--- src/app/router.js
+++ src/app/router.js
@@ -7,14 +7,19 @@ import VueRouter from 'vue-router';
 
 Vue.use( VueRouter );
 
-// Gather routes from modules
+// Gather routes from pages
 let routes = [];
 routes = routes.concat( require("@pages/home/routes.js") );
 
 // Create router instance
-let router = new VueRouter( { 	"routes" : routes,
+let router = new VueRouter( { 	// Register all routes 
+								"routes" : routes,
+
 								// set scroll position to top of page after each route navigation
 								scrollBehavior (to, from, savedPosition) { return { x: 0, y: 0 } },
+
+								// "history" mode removes the # from URLs. *note* you'll need to add rewrite rules server side
+								// mode: "history",
 							} );
 
 ///////////////////////////////////////////////////////////
